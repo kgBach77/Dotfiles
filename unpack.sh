aa0a688382e6db466fa8643a88e0e0136ae9b952
Kyle Goldbeck <kyle.goldbeck@gmail.com>
2020-12-31 10:18:45 -0800

    Added fonts folder

--- unpack.sh
+++ unpack.sh
@@ -1,3 +1,9 @@
 # A program to unpack my dotfiles on a new system.
 # Mostly to just move around my emacs packages to
 # get a new environment working easier
+
+# Take all the files in the Dotfiles directory and copy them to the home directory,
+# overriding anything that is already there.
+# Then restart bash
+# cp * ~/
+
---
+++ unpack.sh~
@@ -0,0 +1,3 @@
+# A program to unpack my dotfiles on a new system.
+# Mostly to just move around my emacs packages to
+# get a new environment working easier
